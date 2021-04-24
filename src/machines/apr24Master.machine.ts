6950ca0b61cb0ee34b2246a26ea4e8077dadc77f
Johnny Noble <github-a3bd@johnny.net.au>
2021-04-24 12:46:03 +1000

    Don’t know why he got missed, but

--- src/machines/apr24Master.machine.ts
+++ src/machines/apr24Master.machine.ts
@@ -1,5 +1,5 @@
 import { Machine } from "@xstate/compiled";
-import userbase from "userbase-js";
+import userbase, { UserResult } from "userbase-js";
 
 interface Context {}
 
@@ -7,16 +7,38 @@ type Event =
   | { type: "TESTER" }
   | { type: "TRY_SIGNIN" }
   | { type: "TRY_SIGNOUT" }
-  | { type: "REPORT_SIGNIN_SUCCESS" }
-  | { type: "REPORT_SIGNIN_FAILURE" };
+  | { type: "REPORT_SIGNIN_SUCCESS"; user: UserResult }
+  | { type: "REPORT_SIGNIN_FAILURE" }
+  | { type: "REPORT_SIGNOUT_SUCCESS" }
+  | { type: "REPORT_SIGNOUT_FAILURE" }
+  | { type: "WEIRD_ERROR"; error: any };
 
 export const apr24MasterMachine = Machine<Context, Event, "apr24MasterMachine">(
   {
     id: "apr24MasterMachine",
     initial: "init",
     states: {
-      init: {},
-      signedIn: {},
+      init: {
+        invoke: {
+          src: "userbaseInit",
+        },
+        on: {
+          REPORT_SIGNIN_SUCCESS: "signedIn",
+          REPORT_SIGNIN_FAILURE: "notSignedIn",
+        },
+      },
+      signedIn: {
+        on: {
+          TRY_SIGNOUT: {
+            target: "tryingSignOut",
+          },
+        },
+      },
+      tryingSignOut: {
+        invoke: {
+          src: "userbaseSignOut",
+        },
+      },
       notSignedIn: {},
     },
   },
@@ -32,12 +54,32 @@ export const apr24MasterMachine = Machine<Context, Event, "apr24MasterMachine">(
             appId: "37c7462e-f79c-4ef3-bdb0-55968a34d572",
           })
           .then((session) => {
+            /**
+             * This only tells us that the SDK initialised successfully, *not*
+             * that there is an active user. For that we need `session.user`
+             * to contain a user object.
+             */
             console.log(session);
-            sendBack({ type: "REPORT_SIGNIN_SUCCESS" });
+            if (session.user) {
+              sendBack({ type: "REPORT_SIGNIN_SUCCESS", user: session.user });
+            } else {
+              sendBack({ type: "WEIRD_ERROR", error: session });
+            }
           })
           .catch((error) => {
             console.log(error);
             sendBack({ type: "REPORT_SIGNIN_FAILURE" });
+          });
+      },
+      userbaseSignOut: () => (sendBack: (event: Event) => void) => {
+        userbase
+          .signOut()
+          .then(() => {
+            sendBack({ type: "REPORT_SIGNOUT_SUCCESS" });
+          })
+          .catch((error) => {
+            console.log(error);
+            sendBack({ type: "REPORT_SIGNOUT_FAILURE" });
           });
       },
     },
