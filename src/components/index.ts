5b54873b6ea2366f4834fb1e1db52379dc5e4ee7
Johnny Noble <github-a3bd@johnny.net.au>
2021-07-05 10:27:37 +1000

    New /components/index exporter

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+// @ts-nocheck
+import { databaseMachine } from "./app/DatabaseMachine/database.machine";
+import { DatabaseMachine } from "./app/DatabaseMachine/DatabaseMachine";
+
+export { DatabaseMachine, databaseMachine };
