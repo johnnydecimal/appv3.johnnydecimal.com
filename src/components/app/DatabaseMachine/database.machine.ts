e5d0ee8a4636b0a67769e29bb6bc23f2fb2c5490
Johnny Noble <github-a3bd@johnny.net.au>
2021-07-14 17:46:35 +1000

    Starting again with database.machine

--- src/components/app/DatabaseMachine/database.machine.ts
+++ src/components/app/DatabaseMachine/database.machine.ts
@@ -14,7 +14,7 @@ interface JDUserbaseItem extends Item {
   item: JDItem;
 }
 
-interface DatabaseMachineContext {
+export interface DatabaseMachineContext {
   /**
    * The array of the user's available database objects, returned by Userbase.
    * The `databaseName` property on each object is the 3-digit JD project code,
@@ -34,38 +34,39 @@ interface DatabaseMachineContext {
   error: any;
 }
 
-type DatabaseMachineEvent =
+export type DatabaseMachineEvent =
   /**
    * ubGetDatabases returns GOT ARRAY OF DATABASES as long as the connection to
    * Userbase was successful. `databases` could be an empty array.
    */
-  | { type: "GOT ARRAY OF DATABASES"; databases: Database[] }
+  | { type: "GOT DATABASES"; databases: Database[] }
 
   /**
    * countingDatabases returns depending on the length of the databases array.
    */
-  | { type: "ZERO DATABASES DETECTED" }
-  | { type: "ONE OR MORE DATABASES DETECTED" }
+  // | { type: "ZERO DATABASES DETECTED" }
+  // | { type: "ONE OR MORE DATABASES DETECTED" }
 
   /**
    * ubCreateFirstDatabase
    */
-  | { type: "FIRST DATABASE CREATED" }
+  // | { type: "FIRST DATABASE CREATED" }
 
   /**
    * ubOpenDatabase.
    */
   // The changeHandler fires this.
-  | { type: "USERBASEITEMS UPDATED"; userbaseItems: Item[] }
+  // | { type: "USERBASEITEMS UPDATED"; userbaseItems: Item[] }
 
   // When we want to create a new project.
-  | { type: "CREATE PROJECT"; projectNumber: string }
+  // | { type: "CREATE PROJECT"; projectNumber: string }
 
   // When we open a database.
-  | { type: "DATABASE OPENED" }
+  // | { type: "DATABASE OPENED" }
 
   // Testing/building
   | { type: "TEST" }
+
   // Errors
   | { type: "ERROR"; error: UserbaseError };
 
@@ -77,127 +78,31 @@ export const databaseMachine = Machine<
 >(
   {
     id: "databaseMachine",
-    initial: "getDatabases",
+    // initial: "getDatabases",
+    type: "parallel",
     context: {
       databases: [],
       currentProject: "",
       userbaseItems: [],
       error: undefined,
     },
-    on: {
-      "USERBASEITEMS UPDATED": {
-        /**
-         * ubOpenDatabase.changeHandler sends this event whenever it receives
-         * updated data.
-         */
-        actions: [
-          assign({
-            userbaseItems: (context, event) => event.userbaseItems,
-          }),
-        ],
-      },
-      "CREATE PROJECT": {
-        actions: [
-          assign({
-            currentProject: (context, event) => event.projectNumber,
-          }),
-        ],
-        target: "#databaseMachine.openDatabase",
-      },
-      ERROR: {
-        target: "#databaseMachine.error",
-        actions: [
-          (context, event) => console.log("ERROR.event:", event),
-          assign({
-            error: (context, event) => event.error,
-          }),
-        ],
-      },
-    },
     states: {
-      getDatabases: {
+      databaseGetter: {
         type: "compound",
-        initial: "init",
+        initial: "gettingDatabases",
         states: {
-          init: {
+          gettingDatabases: {
             invoke: {
               src: "ubGetDatabases",
             },
             on: {
-              "GOT ARRAY OF DATABASES": {
-                actions: [
-                  assign({
-                    databases: (context, event) => event.databases,
-                  }),
-                  assign({
-                    currentProject: (context, event) => {
-                      if (event.databases[0]) {
-                        return event.databases[0].databaseName;
-                      } else {
-                        return "";
-                      }
-                    },
-                  }),
-                ],
-                target: "countingDatabases",
-              },
+              "GOT DATABASES": "idle",
             },
           },
-          countingDatabases: {
-            /**
-             * We got zero or some databases. These are our projects.
-             *
-             * Figure out how many and act accordingly.
-             */
-            invoke: {
-              src: "countDatabases",
-            },
-            on: {
-              "ZERO DATABASES DETECTED": {
-                target: "creatingFirstDatabase",
-              },
-              "ONE OR MORE DATABASES DETECTED": {
-                target: "#databaseMachine.openDatabase",
-              },
-            },
-          },
-          creatingFirstDatabase: {
-            invoke: {
-              src: "ubCreateFirstDatabase",
-            },
-            on: {
-              "FIRST DATABASE CREATED": {
-                target: "init",
-              },
-            },
-          },
+          idle: {},
         },
       },
-      openDatabase: {
-        type: "compound",
-        initial: "openingDatabase",
-        /**
-         * This is where the Userbase changeHandler gets set up. If we exit this
-         * state the handler gets killed/ignored.
-         */
-        invoke: {
-          src: "ubOpenDatabase",
-        },
-        states: {
-          openingDatabase: {
-            on: {
-              "DATABASE OPENED": "databaseIsOpen",
-            },
-          },
-          databaseIsOpen: {},
-        },
-      },
-      error: {
-        /**
-         * Top-level error state. Calling ERROR anywhere will bring us here.
-         */
-        type: "final",
-      },
+      databaseOpener: {},
     },
   },
   {
@@ -209,62 +114,209 @@ export const databaseMachine = Machine<
         userbase
           .getDatabases()
           .then(({ databases }) => {
-            sendBack({ type: "GOT ARRAY OF DATABASES", databases });
+            sendBack({ type: "GOT DATABASES", databases });
           })
-          .catch((error) => sendBack({ type: "ERROR", error }));
-      },
-      countDatabases: (context: DatabaseMachineContext) => (sendBack: any) => {
-        if (context.databases.length === 0) {
-          sendBack({ type: "ZERO DATABASES DETECTED" });
-        } else {
-          sendBack({ type: "ONE OR MORE DATABASES DETECTED" });
-        }
-      },
-      ubCreateFirstDatabase: () => (sendBack: any) => {
-        userbase
-          .openDatabase({
-            databaseName: "001",
-            changeHandler: () => {
-              /**
-               * This changeHandler isn't needed, but is required by Userbase.
-               * We exit the state that invoked this service immediately, so
-               * anything created by this service will be ignored by XState.
-               *
-               * This function might be a minor memory leak? Which is why it
-               * doesn't do anything.
-               */
-            },
-          })
-          .then(() => {
-            sendBack({ type: "FIRST DATABASE CREATED" });
-          })
-          .catch((error) => sendBack({ type: "ERROR", error }));
-      },
-      ubOpenDatabase: (context: DatabaseMachineContext) => (sendBack: any) => {
-        if (context.currentProject === "") {
-          sendBack({
-            type: "ERROR",
-            error: "ubOpenDatabase called while context.currentProject === ''",
-          });
-          return;
-        }
-        userbase
-          .openDatabase({
-            databaseName: context.currentProject,
-            changeHandler: (userbaseItems) => {
-              sendBack({
-                type: "USERBASEITEMS UPDATED",
-                userbaseItems,
-              });
-            },
-          })
-          .then(() => {
-            sendBack({ type: "DATABASE OPENED" });
-          })
-          .catch((error) => {
-            sendBack({ type: "ERROR", error });
-          });
+          .catch((error: UserbaseError) => sendBack({ type: "ERROR", error }));
       },
     },
   }
 );
+
+// export const databaseMachine = Machine<
+//   DatabaseMachineContext,
+//   DatabaseMachineEvent,
+//   "databaseMachine"
+// >(
+//   {
+//     id: "databaseMachine",
+//     initial: "getDatabases",
+//     context: {
+//       databases: [],
+//       currentProject: "",
+//       userbaseItems: [],
+//       error: undefined,
+//     },
+//     on: {
+//       "USERBASEITEMS UPDATED": {
+//         /**
+//          * ubOpenDatabase.changeHandler sends this event whenever it receives
+//          * updated data.
+//          */
+//         actions: [
+//           assign({
+//             userbaseItems: (context, event) => event.userbaseItems,
+//           }),
+//         ],
+//       },
+//       "CREATE PROJECT": {
+//         actions: [
+//           assign({
+//             currentProject: (context, event) => event.projectNumber,
+//           }),
+//         ],
+//         target: "#databaseMachine.openDatabase",
+//       },
+//       ERROR: {
+//         target: "#databaseMachine.error",
+//         actions: [
+//           (context, event) => console.log("ERROR.event:", event),
+//           assign({
+//             error: (context, event) => event.error,
+//           }),
+//         ],
+//       },
+//     },
+//     states: {
+//       getDatabases: {
+//         type: "compound",
+//         initial: "init",
+//         states: {
+//           init: {
+//             invoke: {
+//               src: "ubGetDatabases",
+//             },
+//             on: {
+//               "GOT ARRAY OF DATABASES": {
+//                 actions: [
+//                   assign({
+//                     databases: (context, event) => event.databases,
+//                   }),
+//                   assign({
+//                     currentProject: (context, event) => {
+//                       if (event.databases[0]) {
+//                         return event.databases[0].databaseName;
+//                       } else {
+//                         return "";
+//                       }
+//                     },
+//                   }),
+//                 ],
+//                 target: "countingDatabases",
+//               },
+//             },
+//           },
+//           countingDatabases: {
+//             /**
+//              * We got zero or some databases. These are our projects.
+//              *
+//              * Figure out how many and act accordingly.
+//              */
+//             invoke: {
+//               src: "countDatabases",
+//             },
+//             on: {
+//               "ZERO DATABASES DETECTED": {
+//                 target: "creatingFirstDatabase",
+//               },
+//               "ONE OR MORE DATABASES DETECTED": {
+//                 target: "#databaseMachine.openDatabase",
+//               },
+//             },
+//           },
+//           creatingFirstDatabase: {
+//             invoke: {
+//               src: "ubCreateFirstDatabase",
+//             },
+//             on: {
+//               "FIRST DATABASE CREATED": {
+//                 target: "init",
+//               },
+//             },
+//           },
+//         },
+//       },
+//       openDatabase: {
+//         type: "compound",
+//         initial: "openingDatabase",
+//         /**
+//          * This is where the Userbase changeHandler gets set up. If we exit this
+//          * state the handler gets killed/ignored.
+//          */
+//         invoke: {
+//           src: "ubOpenDatabase",
+//         },
+//         states: {
+//           openingDatabase: {
+//             on: {
+//               "DATABASE OPENED": "databaseIsOpen",
+//             },
+//           },
+//           databaseIsOpen: {},
+//         },
+//       },
+//       error: {
+//         /**
+//          * Top-level error state. Calling ERROR anywhere will bring us here.
+//          */
+//         type: "final",
+//       },
+//     },
+//   },
+//   {
+//     services: {
+//       ubGetDatabases: () => (sendBack: any) => {
+//         /**
+//          * Get the array of databases. Is always returned, can be empty.
+//          */
+//         userbase
+//           .getDatabases()
+//           .then(({ databases }) => {
+//             sendBack({ type: "GOT ARRAY OF DATABASES", databases });
+//           })
+//           .catch((error) => sendBack({ type: "ERROR", error }));
+//       },
+//       countDatabases: (context: DatabaseMachineContext) => (sendBack: any) => {
+//         if (context.databases.length === 0) {
+//           sendBack({ type: "ZERO DATABASES DETECTED" });
+//         } else {
+//           sendBack({ type: "ONE OR MORE DATABASES DETECTED" });
+//         }
+//       },
+//       ubCreateFirstDatabase: () => (sendBack: any) => {
+//         userbase
+//           .openDatabase({
+//             databaseName: "001",
+//             changeHandler: () => {
+//               /**
+//                * This changeHandler isn't needed, but is required by Userbase.
+//                * We exit the state that invoked this service immediately, so
+//                * anything created by this service will be ignored by XState.
+//                *
+//                * This function might be a minor memory leak? Which is why it
+//                * doesn't do anything.
+//                */
+//             },
+//           })
+//           .then(() => {
+//             sendBack({ type: "FIRST DATABASE CREATED" });
+//           })
+//           .catch((error) => sendBack({ type: "ERROR", error }));
+//       },
+//       ubOpenDatabase: (context: DatabaseMachineContext) => (sendBack: any) => {
+//         if (context.currentProject === "") {
+//           sendBack({
+//             type: "ERROR",
+//             error: "ubOpenDatabase called while context.currentProject === ''",
+//           });
+//           return;
+//         }
+//         userbase
+//           .openDatabase({
+//             databaseName: context.currentProject,
+//             changeHandler: (userbaseItems) => {
+//               sendBack({
+//                 type: "USERBASEITEMS UPDATED",
+//                 userbaseItems,
+//               });
+//             },
+//           })
+//           .then(() => {
+//             sendBack({ type: "DATABASE OPENED" });
+//           })
+//           .catch((error) => {
+//             sendBack({ type: "ERROR", error });
+//           });
+//       },
+//     },
+//   }
+// );
