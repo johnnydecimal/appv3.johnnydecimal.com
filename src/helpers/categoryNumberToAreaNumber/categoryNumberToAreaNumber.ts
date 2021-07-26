2e054779af49068941c8945c411c9b5cffb73fee
Johnny Noble <github-a3bd@johnny.net.au>
2021-07-26 14:36:28 +1000

    WIP ubItoInternalJDSystem done, not tested/integrated

---
+++ src/helpers/categoryNumberToAreaNumber/categoryNumberToAreaNumber.ts
@@ -0,0 +1,28 @@
+// === Types    ===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===
+import { JDAreaNumbers, JDCategoryNumbers } from "@types";
+
+// === Main ===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===-===
+export const categoryNumberToAreaNumber = (
+  category: JDCategoryNumbers
+): JDAreaNumbers => {
+  /**
+   * We ts-ignore later, but testing shows that it isn't possible to write a
+   * call to this function that doesn't take a valid JDCategoryNumber, so we're
+   * not doing any further testing here.
+   */
+  const categoryFirstNumberToAreaDictionary = {
+    "0": "00-09",
+    "1": "10-19",
+    "2": "20-29",
+    "3": "30-39",
+    "4": "40-49",
+    "5": "50-59",
+    "6": "60-69",
+    "7": "70-79",
+    "8": "80-89",
+    "9": "90-99",
+  };
+  const firstNumber = category.charAt(0);
+  // @ts-ignore
+  return categoryFirstNumberToAreaDictionary[firstNumber];
+};
--- src/helpers/userbaseItemsToInternalJdSystem/userbaseItemsToInternalJdSystem.ts
+++ src/helpers/userbaseItemsToInternalJdSystem/userbaseItemsToInternalJdSystem.ts
@@ -6,7 +6,15 @@
 //   JDIdNumbers,
 // } from "@types";
 
-import { InternalJDSystem, JDProjectNumbers, UserbaseItem } from "@types";
+import { categoryNumberToAreaNumber } from "helpers/categoryNumberToAreaNumber/categoryNumberToAreaNumber";
+
+import {
+  InternalJDSystem,
+  JDProjectNumbers,
+  JDAreaNumbers,
+  UserbaseItem,
+  JDCategoryNumbers,
+} from "@types";
 
 /**
  * # userbaseItemsToInternalJdSystem
@@ -38,6 +46,92 @@ export const userbaseItemsToInternalJdSystem = (
       areas: {},
     },
   };
+
+  /**
+   * Set up variables that we'll use to loop over `userbaseItems[]`.
+   */
+  var i = 0,
+    len = userbaseItems.length;
+
+  /**
+   * First pass: get all of the items which are an area.
+   *
+   * Note that testing shows that this entire area/category/ID generation from
+   * a full 10,110-record userbaseItems[] takes <50ms. So we're not going to
+   * bother trying to optimise by exiting early when we have all of the areas,
+   * for example.
+   */
+  while (i < len) {
+    const item = userbaseItems[i].item;
+    if (item.jdType === "area") {
+      const areaNumber = userbaseItems[i].item.jdNumber as JDAreaNumbers;
+      const areaTitle = userbaseItems[i].item.jdTitle;
+      /**
+       * The forcing-TS-to-believe! here assumes that we haven't screwed up
+       * elsewhere and somehow added an area to a project that doesn't exist.
+       * Is that even possible with the current model? I don't think so.
+       */
+      // prettier-ignore
+      internalJDSystem[currentProjectNumber]!
+        .areas[areaNumber] = {
+          title: areaTitle,
+          categories: {},
+        };
+    }
+    i++;
+  }
+
+  /**
+   * Second pass: get the categories.
+   */
+  i = 0;
+  while (i < len) {
+    const item = userbaseItems[i].item;
+    if (item.jdType === "category") {
+      const categoryNumber = item.jdNumber as JDCategoryNumbers;
+      const categoryTitle = userbaseItems[i].item.jdTitle;
+      const areaNumber = categoryNumberToAreaNumber(categoryNumber);
+      /**
+       * The forcing-TS-to-believe! here assumes that we haven't screwed up
+       * elsewhere and created a category without an associated area, or deleted
+       * an area with existing categories. The functions where we do that will
+       * need to be 100%.
+       */
+      // prettier-ignore
+      internalJDSystem[currentProjectNumber]!
+        .areas[areaNumber]!
+        .categories[categoryNumber] = {
+          title: categoryTitle,
+          ids: {},
+        };
+    }
+    i++;
+  }
+
+  /**
+   * Third pass: get the IDs.
+   */
+  i = 0;
+  while (i < len) {
+    const item = userbaseItems[i].item;
+    if (item.jdType === "id") {
+      const idNumber = item.jdNumber;
+      const idTitle = item.jdTitle;
+      const categoryNumber: JDCategoryNumbers = item.jdNumber.substr(
+        0,
+        2
+      ) as JDCategoryNumbers;
+      const areaNumber = categoryNumberToAreaNumber(categoryNumber);
+      // prettier-ignore
+      internalJDSystem[currentProjectNumber]!
+        .areas[areaNumber]!
+        .categories[categoryNumber]!
+        .ids[idNumber] = {
+          title: idTitle,
+        };
+    }
+    i++;
+  }
 
   return internalJDSystem;
 };
